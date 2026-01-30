78daf568efba63060edda7de913c19cd59319683
Sriti Singh <sritiwebdeveloper@gmail.com>
2026-01-30 23:19:06 +0530

    Initial GlowUp Tracker commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,64 +2,36 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <main className="min-h-screen bg-[#dab2ff] p-4">
+      <div className="flex items-center justify-center py-4">
         <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          src="/selfcare.png"
+          alt="Self Care logo"
           width={100}
           height={20}
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+      </div>
+      <h1 className="text-center text-white text-2xl">Hi there,</h1>
+      <h2 className="text-center text-white text-2xl">I'm Glow Up Tracker</h2>
+      <p className="text-[#3a2d3087] text-center mt-5">
+            Your new daily 
+        </p>
+        <p className="text-[#3a2d3087] text-center mb-6">
+        self-care companion ✨
+        </p>
+      <div className="action-buttons-triggers py-4">
+        <div className="w-full max-w-sm bg-white text-center p-4 mb-8 rounded-3xl shadow-xl">
+          <a href="#" className="text-sm/6 font-semi-bold mt-12  text-center mb-4">
+            Hi, GLOWUP TRACKER
+          </a>          
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+        <a href="#" className="text-gray-500 font-semi-bold ml-[20px] text-center uppercase">
+            I already have an account
+        </a>
+      </div>
+
+    </main>
   );
 }
