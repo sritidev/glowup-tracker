0b6610712265296f7c6dd547ab76d753970b8880
Sriti Singh <sritiwebdeveloper@gmail.com>
2026-02-04 12:06:10 +0530

    creating mood component

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,9 @@
+import MoodSelector from "../components/mood/MoodSelector";
+
+export default function Dashboard (){
+    return(
+        <main className="min-h-screen bg-[#f7efe7] p-4">
+            <MoodSelector/>
+        </main>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#dab2ff] p-4">
+    <main className="min-h-screen bg-[#dab2ff7d] p-4">
       <div className="flex items-center justify-center py-4">
         <Image
           className="dark:invert"
@@ -23,18 +24,16 @@ export default function Home() {
       </p>
       <div className="action-buttons-triggers py-4">
         <div className="w-full max-w-sm bg-white text-center p-4 mb-8 rounded-3xl shadow-xl">
-          <a href="#" className="text-sm/6 font-semi-bold mt-12  text-center mb-4">
-            Hi, GLOWUP TRACKER
-          </a>
+         
+          <Link className="text-sm/6 font-semi-bold mt-12  text-center mb-4" 
+          href="/dashboard"> Hi, GLOWUP TRACKER</Link>
         </div>
         <div className="flex items-center justify-center">
-          <a href="#" className="text-gray-500 font-semi-bold text-center uppercase">
-            I already have an account
-          </a>
+          <Link className="text-gray-500 font-semi-bold text-center uppercase" 
+          href="/dashboard">I already have an account</Link>
         </div>
-
       </div>
-
     </main>
+    
   );
 }
